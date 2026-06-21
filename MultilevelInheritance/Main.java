0dbf79a352497d1163ebef602b8f162f8e1f8cfa
Bharathkh-04 <bharathkh04@gmail.com>
2026-06-21 14:20:17 +0530

    Create Main.java

---
+++ MultilevelInheritance/Main.java
@@ -0,0 +1,13 @@
+package com.bh.oops.Inheritance;
+
+public class Main {
+public static void main(String[] args)
+{
+	Boxprice w4=new Boxprice();
+	System.out.println(w4.length);
+	
+	Boxprice w5=new Boxprice(w1);
+	System.out.println(w5.weight);
+	
+}
+}
