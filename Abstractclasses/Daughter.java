0e75a96fe0da8711c3cf33ba8e3fd68adeb6a1ea
Bharathkh-04 <bharathkh04@gmail.com>
2026-06-25 01:39:49 +0530

    Create Daughter.java

---
+++ Abstractclasses/Daughter.java
@@ -0,0 +1,17 @@
+package abstractclass;
+
+public class Daughter extends AbstractClasses{
+	@Override
+	void person()
+	{
+		System.out.println("my career is doctor");
+	}
+
+	@Override
+	void Age()
+	{
+		System.out.println("my age is 24");
+	}
+
+
+}
