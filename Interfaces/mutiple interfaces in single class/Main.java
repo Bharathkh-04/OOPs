ae50ffe8865daa4f5d36b29e9f3fe042759e19a6
Bharathkh-04 <bharathkh04@gmail.com>
2026-06-24 14:50:36 +0530

    Create Main.java

---
+++ Interfaces/mutiple interfaces in single class/Main.java
@@ -0,0 +1,14 @@
+package Interfacesdemo;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Car obj=new Car();
+	obj.start();
+	
+	Engine obj1=new Car();
+	obj1.accelerate();
+	
+
+}
+}
