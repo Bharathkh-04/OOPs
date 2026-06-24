fc4526e6e640e915c8bb23d9c828604ceebde168
Bharathkh-04 <bharathkh04@gmail.com>
2026-06-25 01:36:06 +0530

    Create ObjectPrint.java

---
+++ Polymorphism/ObjectPrint.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class ObjectPrint {
+int num=7;
+
+
+//@Override
+//public String toString() {
+//	return "ObjectPrint [num=" + num + "]";
+//}
+
+
+public static void main(String[] args) {
+	ObjectPrint obj=new ObjectPrint();
+	System.out.println(obj);
+}
+
+}
