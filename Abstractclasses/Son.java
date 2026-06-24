07f96b05cab026bc480c7310a69597e8244529c4
Bharathkh-04 <bharathkh04@gmail.com>
2026-06-25 01:40:40 +0530

    Create Son.java

---
+++ Abstractclasses/Son.java
@@ -0,0 +1,18 @@
+package abstractclass;
+
+public class Son extends AbstractClasses {
+
+	@Override
+	void person() {
+		// TODO Auto-generated method stub
+		System.out.println("My careeri is professor");
+	}
+
+	@Override
+	void Age() {
+		// TODO Auto-generated method stub
+		System.out.println("my age is 45");
+	}
+	
+
+}
